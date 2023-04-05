1f0f64cb2c25fcc12c82b9304d9325916581869e
Niloy Sarker <niloysarker.cs@gmail.com>
2023-04-05 19:27:55 +0600

    CS Fundamentals

--- Introduction to Programming Language & Problem Solving Part-I/Weak 01-Basic C programming concept/Module 2,5-Weak 01 practice day 01/pp_05.c
+++ Introduction to Programming Language & Problem Solving Part-I/Weak 01-Basic C programming concept/Module 2,5-Weak 01 practice day 01/pp_05.c
@@ -1,3 +1,23 @@
+/*
+Suppose your friend has told you that she will buy a Gucci Bag if she has 10 thousand taka or more.
+ Otherwise if she has 5 thousand taka or more, she will buy a Levis Bag. Otherwise she will buy Something from New Market.
+  She also told you that, if she could buy the Gucci bag and she has more than 20 thousand taka she will also buy a Gucci Belt.
+
+Now, if you know the amount of money she has, can you tell which item/items she will buy?
+See the sample input and output for more clarification.
+
+Sample Input
+Sample Output
+20000
+Gucci Bag
+6500
+Levis Bag
+200
+Something
+25400
+Gucci Bag
+Gucci Belt
+*/
 #include <stdio.h>
 
 int main() {
