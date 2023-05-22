bb886f1c43268c5d7be4cacdf87e4f6fbb2e3575
Niloy Sarker <niloysarker.cs@gmail.com>
2023-05-23 00:29:41 +0600

    CS Fundamentals C++ W1-M2

---
+++ 02.Introduction to C++ For DSA/Weak 01-Introduction to C++/Module 02-Dynamic Memory Allocation/02_dynamic_data.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+//     int *a=new int;
+//     *a=10;
+//     cout<<*a<<endl;
+ float *a=new float;
+    *a=10.26;
+    cout<<*a<<endl;
+    return 0;
+}
---
+++ 02.Introduction to C++ For DSA/Weak 01-Introduction to C++/Module 02-Dynamic Memory Allocation/04_dynamic_array.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int *ar=new int[100];
+    for (int i = 0; i < 5; i++)
+    {
+      cin>>ar[i];
+    }
+     for(int i = 0; i < 5; i++)
+    {
+      std::cout << ar[i]<<" ";
+    }
+    return 0;
+}
